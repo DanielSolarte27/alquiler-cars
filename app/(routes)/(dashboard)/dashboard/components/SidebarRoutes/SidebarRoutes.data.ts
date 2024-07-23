b29919c2e8619b37eb130bc1105063a8b38f5abc
DanielSolarte27 <danielsolarte27@gmail.com>
2024-07-23 18:43:49 -0500

    Implementacion de rutas

---
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.data.ts
@@ -0,0 +1,25 @@
+import { Calendar, Car, Heart, NotepadText } from "lucide-react";
+
+export const dataGeneralSidebar = [
+  {
+    icon: Car,
+    label: "Cars",
+    href: "/dashboard",
+  },
+  {
+    icon: Calendar,
+    label: "Cars Reserves",
+    href: "/reserves",
+  },
+  {
+    icon: Heart,
+    label: "Loved-Cars",
+    href: "/loved-cars",
+  },
+];
+
+export const dataAdminSidebar = [
+  {
+    icon: NotepadText
+  }
+]
--- app/(routes)/(dashboard)/layout.tsx
+++ app/(routes)/(dashboard)/layout.tsx
@@ -1,5 +1,15 @@
-export default function LayoutDashboard() {
+import { Sidebar } from "./dashboard/components/Sidebar";
+
+export default function LayoutDashboard({children} : {children: React.ReactNode}) {
   return (
-    <div>LayoutDashboard</div>
+    <div className="flex w-full h-full">
+      <div className="hidden h-full xl:block w-80 xl:fixed">
+        <Sidebar/>
+      </div>
+      <div className="w-full h-full xl:ml-80">
+        Navbar Dashboard...
+        <div className="p-6 h-max">{children}</div>
+      </div>
+    </div>
   )
 }
